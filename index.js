a664f6974f726a1ab8629a18339d09179ea2c7b3
Ngaremaina <ngaremaina4@gmail.com>
2023-03-27 19:11:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+document.addEventListener('DOMContentLoaded', submitData = (myname, myemail) =>{
+ 
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: myname,
+            email: myemail
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        
+    })
+    .catch(error => {
+        let errorMessage = document.createElement('p')
+        errorMessage.textContent = error.message
+        document.body.append(errorMessage)
+    })
+})
+
